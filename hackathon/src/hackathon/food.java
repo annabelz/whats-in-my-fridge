52703c216751ac10fc1855332a7af1e8cf0ec1ae
Almseenoz <103285272+Almseenoz@users.noreply.github.com>
2023-01-28 15:06:25 -0500

    Add files via upload

---
+++ hackathon/src/hackathon/food.java
@@ -0,0 +1,40 @@
+package hackathon;
+import java.util.*;
+
+
+public class food {
+	
+	// Attributes fetched from grocery database API
+	private String foodName;
+	private long codeBar; //used for identification in the database
+	private Date storeExpiry;
+	private String dietaryType; 
+
+	// Attribute initialized by the app (maybe web-scraping, maybe just manually from a single source. 
+	private Date actualExpiry; 
+	
+	
+	public food(/* API parameters */ )
+	{
+		
+	}
+	
+	// Make this a persistent class. 
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+		
+	public String getFoodName()
+	{
+		return foodName;
+	}
+	
+
+}
